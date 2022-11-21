eb4f15fc3f1cb347c04b48fe9ab1de78a34c9e06
chithranjan kumar <kumarchithranjan@gmail.com>
2022-11-21 11:13:45 +0530

    added new feature for updating user email data

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 
 import './App.css';
 import Layout from './components/Layout';
 import Login from './pages/Login';
 import Home from './pages/Home';
-import { replaceMail } from './store/mail-actions';
 
 function App() {
+  
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const firstTime = useSelector((state) => state.mail.firstTime);
-  const dispatch = useDispatch();
-
-  if (isLoggedIn && firstTime) {
-    const loggedUserEmail = JSON.parse(localStorage.getItem('idToken')).email;
-    const emailUrl = loggedUserEmail.replace('@', '').replace('.', '');
-    dispatch(replaceMail(emailUrl, loggedUserEmail));
-  }
 
   return (
     <Layout>
--- src/store/mail-actions.js
+++ src/store/mail-actions.js
@@ -89,7 +89,7 @@ export const replaceMail = (emailUrl, loggedUserEmail) => {
 // deleting mail
 export const deleteMail = (mail) => {
   const userEmail = JSON.parse(localStorage.getItem('idToken')).email;
-  const emailUrl = userEmail.replace('@','').replace('.','');
+  const emailUrl = userEmail.replace('@', '').replace('.', '');
 
   return async (dispatch) => {
     try {
@@ -104,6 +104,44 @@ export const deleteMail = (mail) => {
 
       if (response.ok) {
         dispatch(mailActions.remove(mail));
+      } else {
+        throw data.error;
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+};
+
+// updating mail
+export const updateMail = (emailUrl, loggedUserEmail, currentMailData) => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        `https://mail-box-client-88922-default-rtdb.firebaseio.com/${emailUrl}.json`
+      );
+
+      const data = await response.json();
+
+      if (response.ok) {
+        if (data.length > currentMailData.length) {
+          let mailData = [];
+          let unreadMessageCount = 0;
+
+          for (let key in data) {
+            mailData = [{ id: key, ...data[key] }, ...mailData];
+            if (data[key].to === loggedUserEmail && data[key].read === false) {
+              unreadMessageCount++;
+            }
+          }
+
+          dispatch(
+            mailActions.replace({
+              mailData: mailData,
+              unreadMessageCount: unreadMessageCount,
+            })
+          );
+        }
       } else {
         throw data.error;
       }
